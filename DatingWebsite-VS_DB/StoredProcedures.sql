5761d6206a15161bac8876b103b7170914e390c4
claired1 <clairedelaney2@gmail.com>
2015-08-20 16:37:52 +0100

    Added SqL stored procedures and view

---
+++ DatingWebsite-VS_DB/StoredProcedures.sql
@@ -0,0 +1,71 @@
+﻿CREATE PROCEDURE uspUserProfile_UserName
+@Name NVarChar(50)
+AS  
+SELECT
+	UserInformation.UserId,
+	UserInformation.UserName,
+	UserInformation.Location,
+	UserInformation.Profession,
+	UserInformation.Gender,
+	UserInformation.AgeRange,
+	UserInformation.SexualOrientation,
+	UserInformation.EyeColour,
+	UserInformation.HairColour,
+	UserInformation.Height,
+	UserInformation.Build
+
+FROM UserInformation
+wHERE UserName=@Name
+
+
+
+CREATE PROCEDURE uspSearchByGender_Orientation
+@Gender NVarChar(50),
+@Orientation NVarChar(50)
+
+AS  
+SELECT
+	UserInformation.UserId,
+	UserInformation.UserName,
+	UserInformation.Location,
+	UserInformation.Profession,
+	UserInformation.Gender,
+	UserInformation.AgeRange,
+	UserInformation.SexualOrientation,
+	UserInformation.EyeColour,
+	UserInformation.HairColour,
+	UserInformation.Height,
+	UserInformation.Build
+
+FROM UserInformation
+wHERE Gender= @Gender
+And
+SexualOrientation=@Orientation
+
+CREATE PROCEDURE uspUserProfile_UserID
+@UserId int
+AS  
+SELECT
+	UserInformation.UserId,
+	UserInformation.UserName,
+	UserInformation.Location,
+	UserInformation.Profession,
+	UserInformation.Gender,
+	UserInformation.AgeRange,
+	UserInformation.SexualOrientation,
+	UserInformation.EyeColour,
+	UserInformation.HairColour,
+	UserInformation.Height,
+	UserInformation.Build
+
+FROM UserInformation
+wHERE UserId=@UserId
+
+
+CREATE VIEW [UserLogin_W_UserName] AS
+SELECT UserInformation.UserName, UserLogin.UserId, UserLogin.Email, UserLogin.Password
+FROM UserInformation
+Left Join  UserLogin
+On UserInformation.UserId = UserLogin.UserId
+
+SElect * From [UserLogin_W_UserName]
