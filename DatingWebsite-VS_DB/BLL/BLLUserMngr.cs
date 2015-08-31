0d50f80b706b3b1dad5b7f45ff0fbb68ecfcad6c
ashorething <ashorething@users.noreply.github.com>
2015-08-31 11:01:33 +0100

    Added CreateUser to BLL

--- DatingWebsite-VS_DB/BLL/BLLUserMngr.cs
+++ DatingWebsite-VS_DB/BLL/BLLUserMngr.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -40,7 +40,7 @@ namespace BLL
             {
                 user = new UserModel(userID, username, password);
             }
-            
+
             return user;
         }
 
